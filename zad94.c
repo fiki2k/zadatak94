93a0ced0ccfcb3638313ed42cca0092757a8d4ec
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-02-14 17:46:48 +0100

    Add files via upload

---
+++ zad94.c
@@ -0,0 +1,15 @@
+//Isprintajte sve ASCII znakove na ekran. 
+
+#include<stdio.h>
+ 
+int main() {
+   int i;
+   char ch;
+ 	
+	// Petlja zavrsava u 256 jer toliko ima znakova u ascii tablici. 	
+   for (i = 0; i < 256; i++) {
+      printf("%c ", ch);
+      ch = ch + 1;
+   }
+   return 0;
+}
